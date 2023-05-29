75482b678926987b1740d3b8e581077589ea316b
LeeLiao <leeliaou@gmail.com>
2023-05-29 17:51:39 +0800

    change root NotionID

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '8737c09d277649cfae39eb8b6762f7bb',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Hands-On',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Leeliao',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
